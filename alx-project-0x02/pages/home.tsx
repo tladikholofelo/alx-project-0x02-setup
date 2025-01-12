ab9a3bff2d0531faf5720df38020d7de57107d68
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2025-01-12 21:47:08 +0200

    Create pages/home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,13 @@
+// pages/home.tsx
+import Header from '../components/layout/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to the Home Page!</h1>
+    </div>
+  );
+};
+
+export default Home;
