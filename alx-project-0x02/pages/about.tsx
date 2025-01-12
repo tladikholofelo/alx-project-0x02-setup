6825df6827e3497248dfe4b93226ba005583e363
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2025-01-12 21:56:29 +0200

    Create pages/about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,13 @@
+// pages/about.tsx
+import Header from '../components/layout/Header';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to the About Page!</h1>
+    </div>
+  );
+};
+
+export default About;
